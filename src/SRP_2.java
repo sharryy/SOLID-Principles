82e981c23f01274b3031dd56615f155e8e08d9aa
Sharryy <ibneadam388@gmail.com>
2021-10-12 03:51:52 +0500

    SRP_2 So So Correct Implemented

--- src/SRP_2.java
+++ src/SRP_2.java
@@ -1,21 +1,9 @@
-/*
-Now this example seems reasonable for small scale project.
-When scope of project increases, it will result into spaghetti
-code as Physical Characteristics of Book are intermingled with
-formatting logic like print-book and core logic like downloading
-a book. So it makes code difficult to read, understand and change.
-Moreover, if there is a requirement to change printing format like
-HTML or JSON or PRETTY Format , then it is difficult to modify as
-all responsibilities are assigned to same class violating SINGLE-RESPONSIBILITY PRINCIPLE.
-let us segregate it into classes and interfaces..
- */
-
 public class SRP_2 {
     public static void main(String[] args) {
-        Book book = new Book("lorem", "ipsum", "color", "met");
-        System.out.println(book.getAuthor());
-        book.downloadBook();
-        book.printPage(book);
+        Book book = new Book("lorem", "ipsum", "color", "met", new DownloadBook());
+        Book book2 = new Book("lorem", "ipsum", "color", "met");
+        book.downloadBook(book);
+        book2.printPage(book2, new PrintXMLOutput());
     }
 }
 
@@ -24,6 +12,7 @@ class Book {
     private String author;
     private String ISBN;
     private String page;
+    public DownloadBook downloadAndPersistBook;
 
     public Book(String title, String author, String ISBN, String page) {
         this.title = title;
@@ -32,21 +21,20 @@ class Book {
         this.page = page;
     }
 
-    public String downloadBook() {
-        /*
-        Code to download book from web
-        and then persist it in local storage
-        using some sort of file handling or DB.
-         */
-        return "Book Downloaded and Saved Locally";
+    public Book(String title, String author, String ISBN, String page, DownloadBook downloadBook) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.page = page;
+        this.downloadAndPersistBook = downloadBook;
     }
 
-    public Object printPage(Book book) {
-        /*
-        Use some third-party software to print
-        whole book or individual pages.
-         */
-        return "PRINTED";
+    public void downloadBook(Book book) {
+        this.downloadAndPersistBook.download(book);
+    }
+
+    public void printPage(Book book, IPrintBook print) {
+        print.printBook(book);
     }
 
     public String getTitle() {
@@ -81,3 +69,43 @@ class Book {
         this.page = page;
     }
 }
+
+class DownloadBook {
+    public String download(Book book) {
+         /*
+        Code to download book from web
+        and then persist it in local storage
+        using some sort of file handling or DB.
+         */
+        return "Book Downloaded and Saved Locally";
+    }
+}
+
+interface IPrintBook {
+    Object printBook(Book book);
+}
+
+class PrintHTMLOutput implements IPrintBook {
+
+    @Override
+    public Object printBook(Book book) {
+        /*
+        Use some third-party software to print
+        whole book or individual pages.
+         */
+        return "HTML OUTPUT PRINTED";
+    }
+}
+
+class PrintXMLOutput implements IPrintBook {
+
+    @Override
+    public Object printBook(Book book) {
+        /*
+        Use some third-party software to print
+        whole book or individual pages.
+         */
+        return "XML OUTPUT PRINTED";
+    }
+}
+
